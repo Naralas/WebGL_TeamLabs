af7bf029dacfeb58079c4f2a1aac2e8c998e3a4f
Ludovic Herbelin <ludovic.herbelin@gmail.com>
2018-11-25 13:17:55 +0100

    Changed for pushing one vertex for each index

--- TP2/Cube.js
+++ TP2/Cube.js
@@ -1,6 +1,6 @@
 class Cube extends SolidShape {
   constructor(vertices, indices, colors, normals, width=1.0, center={x:1.0, y:1.0, z:1.0}, color={r:1.0, g:1.0, b:1.0, a:1.0}) {
-    super(vertices, indices, colors, normals, center, color, 8, 36);
+    super(vertices, indices, colors, normals, center, color, 36, 36);
 
     this.width = width;
 
@@ -10,7 +10,7 @@ class Cube extends SolidShape {
     this.setNormals();
   }
 
-  setVertices(hasBeenInitialized=false) {
+  /* setVertices(hasBeenInitialized=false) {
     this.vertices.splice(this.verticesOffset, // index
                          hasBeenInitialized ? this.numberVertices*3 : 0, // number of elements to remove before pushing
                          this.center.x - this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P0
@@ -22,9 +22,62 @@ class Cube extends SolidShape {
                          this.center.x - this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P6
                          this.center.x - this.width/2, this.center.y + this.width/2, this.center.z - this.width/2   // P7
                       );
+  } */
+
+  setVertices(hasBeenInitialized=false) {
+    this.vertices.splice(this.verticesOffset, // index
+                         hasBeenInitialized ? this.numberVertices*3 : 0, // number of elements to remove before pushing
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P0
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P1
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P2
+
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P1
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P2
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P3
+                         
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P0
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P1
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P6
+
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P6
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P1
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P5
+
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P0                         
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P2
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P6
+
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P2
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P7
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P6
+
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P5
+                         this.center.x - this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P6
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P7
+
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P4
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z - this.width/2,  // P5
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P7
+
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P2
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P3
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P4
+
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P2
+                         this.center.x - this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P7
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P4
+
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P1
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z + this.width/2,  // P3
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P4
+
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z + this.width/2,  // P1
+                         this.center.x + this.width/2, this.center.y + this.width/2, this.center.z - this.width/2,  // P4
+                         this.center.x + this.width/2, this.center.y - this.width/2, this.center.z - this.width/2   // P5
+                      );
   }
 
-  setIndices(hasBeenInitialized=false) {
+  /* setIndices(hasBeenInitialized=false) {
     this.indices.splice(this.indicesOffset, // index,
                         hasBeenInitialized ? this.numberIndices : 0, // number of elements to remove before pushing
                         this.verticesOffset/3+0,
@@ -75,7 +128,60 @@ class Cube extends SolidShape {
                         this.verticesOffset/3+4,
                         this.verticesOffset/3+5
                );
+  } */
+  setIndices(hasBeenInitialized=false) {
+    this.indices.splice(this.indicesOffset, // index,
+                        hasBeenInitialized ? this.numberIndices : 0, // number of elements to remove before pushing
+                        this.verticesOffset/3+0,
+                        this.verticesOffset/3+1,
+                        this.verticesOffset/3+2,
+
+                        this.verticesOffset/3+3,
+                        this.verticesOffset/3+4,
+                        this.verticesOffset/3+5,
+
+                        this.verticesOffset/3+6,
+                        this.verticesOffset/3+7,
+                        this.verticesOffset/3+8,
+
+                        this.verticesOffset/3+9,
+                        this.verticesOffset/3+10,
+                        this.verticesOffset/3+11,
+
+                        this.verticesOffset/3+12,
+                        this.verticesOffset/3+13,
+                        this.verticesOffset/3+14,
+
+                        this.verticesOffset/3+15,
+                        this.verticesOffset/3+16,
+                        this.verticesOffset/3+17,
+
+                        this.verticesOffset/3+18,
+                        this.verticesOffset/3+19,
+                        this.verticesOffset/3+20,
+
+                        this.verticesOffset/3+21,
+                        this.verticesOffset/3+22,
+                        this.verticesOffset/3+23,
+
+                        this.verticesOffset/3+24,
+                        this.verticesOffset/3+25,
+                        this.verticesOffset/3+26,
+
+                        this.verticesOffset/3+27,
+                        this.verticesOffset/3+28,
+                        this.verticesOffset/3+29,
+
+                        this.verticesOffset/3+30,
+                        this.verticesOffset/3+31,
+                        this.verticesOffset/3+32,
+
+                        this.verticesOffset/3+33,
+                        this.verticesOffset/3+34,
+                        this.verticesOffset/3+35
+               );
   }
+
 
   setColors() {
     for (let i=0; i<this.numberVertices; i++)
--- TP2/Tetrahedron.js
+++ TP2/Tetrahedron.js
@@ -1,6 +1,6 @@
 class Tetrahedron extends SolidShape {
   constructor(vertices, indices, colors, normals, width=1.0, center={x:1.0, y:1.0, z:1.0}, color={r:1.0, g:1.0, b:1.0, a:1.0}) {
-    super(vertices, indices, colors, normals, center, color, 4, 12);
+    super(vertices, indices, colors, normals, center, color, 12, 12);
 
     this.width = width;
 
@@ -13,16 +13,31 @@ class Tetrahedron extends SolidShape {
   setVertices(hasBeenInitialized=false) {
     let R = this.width/Math.sqrt(3);
     let r = R/2;
-    this.vertices.splice(this.verticesOffset, // index
+    /*this.vertices.splice(this.verticesOffset, // index
                          hasBeenInitialized ? this.numberVertices*3 : 0, // number of elements to remove before pushing
                          this.center.x - (this.width/2), this.center.y - r, this.center.z - r,  // P0
                          this.center.x + (this.width/2), this.center.y - r, this.center.z - r,  // P1
                          this.center.x,                  this.center.y - r, this.center.z + R,  // P2
                          this.center.x,                  this.center.y + R, this.center.z       // P3
+                      );*/
+    this.vertices.splice(this.verticesOffset, // index
+                         hasBeenInitialized ? this.numberVertices*3 : 0, // number of elements to remove before pushing
+                         this.center.x - (this.width/2), this.center.y - r, this.center.z - r,  // P0
+                         this.center.x + (this.width/2), this.center.y - r, this.center.z - r,  // P1
+                         this.center.x,                  this.center.y - r, this.center.z + R,  // P2
+                         this.center.x + (this.width/2), this.center.y - r, this.center.z - r,  // P1
+                         this.center.x,                  this.center.y - r, this.center.z + R,  // P2
+                         this.center.x,                  this.center.y + R, this.center.z,      // P3
+                         this.center.x - (this.width/2), this.center.y - r, this.center.z - r,  // P0
+                         this.center.x,                  this.center.y - r, this.center.z + R,  // P2
+                         this.center.x,                  this.center.y + R, this.center.z,      // P3
+                         this.center.x - (this.width/2), this.center.y - r, this.center.z - r,  // P0
+                         this.center.x + (this.width/2), this.center.y - r, this.center.z - r,  // P1 
+                         this.center.x,                  this.center.y + R, this.center.z      // P3                           
                       );
   }
 
-  setIndices(hasBeenInitialized=false) {
+ /*  setIndices(hasBeenInitialized=false) {
     this.indices.splice(this.indicesOffset, // index,
                         hasBeenInitialized ? this.numberIndices : 0, // number of elements to remove before pushing
                         this.verticesOffset/3+0,
@@ -40,7 +55,28 @@ class Tetrahedron extends SolidShape {
                         this.verticesOffset/3+0,
                         this.verticesOffset/3+1,
                         this.verticesOffset/3+3
+               ); */
+
+  setIndices(hasBeenInitialized=false) {
+    this.indices.splice(this.indicesOffset, // index,
+                        hasBeenInitialized ? this.numberIndices : 0, // number of elements to remove before pushing
+                        this.verticesOffset/3+0,
+                        this.verticesOffset/3+1,
+                        this.verticesOffset/3+2,
+
+                        this.verticesOffset/3+3,
+                        this.verticesOffset/3+4,
+                        this.verticesOffset/3+5,
+
+                        this.verticesOffset/3+6,
+                        this.verticesOffset/3+7,
+                        this.verticesOffset/3+8,
+
+                        this.verticesOffset/3+9,
+                        this.verticesOffset/3+10,
+                        this.verticesOffset/3+11
                );
+               
   }
 
   setColors() {
